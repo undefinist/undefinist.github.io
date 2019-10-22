bd23b61b5a21f6d1749157056e9742a0e8d18530
Malody Hoe <malodyhoe@gmail.com>
2019-10-23 02:27:05 +0800

    update

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var $grid = $('.grid').masonry({
     // options
-    itemSelector: '.card-outer'
+    itemSelector: '.card-outer',
+    columnWidth: '.col-12.col-sm-6.col-md-4.col-lg-4.col-xl-3'
 });
 
 $grid.imagesLoaded().progress(function() {
     $grid.masonry('layout');
+});
+
+$(window).resize(function() {
+    $grid.masonry();
 });
